e456599e6928f391b5537e8456914b3449069215
Luke1836 <codingluke2023@gmail.com>
2025-03-05 20:32:02 +0530

    Styles added

---
+++ Project-1/first-project/app/meals/[slug]/page.js
@@ -0,0 +1,8 @@
+export default function MealsDynamicPage()
+{
+  return (
+    <section>
+      Meals Dynamic Page
+    </section>
+  )
+}
---
+++ Project-1/first-project/app/meals/share/page.js
@@ -0,0 +1,8 @@
+export default function MealsSharePage()
+{
+  return (
+    <section>
+      Meals Share Page
+    </section>
+  )
+}
