f6f88249290b2678f7592ce270afa7e1f073aeae
Luke1836 <codingluke2023@gmail.com>
2025-03-06 20:05:15 +0530

    Styles added 2

--- Project-1/first-project/app/page.js
+++ Project-1/first-project/app/page.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+
+      <p className="text-stone-100 text-[2rem] text-center block"><Link href="\meals">Explore Meals</Link></p>
+      <p className="text-stone-100 text-[2rem] text-center block"><Link href="\meals/share">Explore Shared Meals</Link></p>
+      <p className="text-stone-100 text-[2rem] text-center block"><Link href="\community">Explore Community</Link></p>
     </main>
   );
 }
